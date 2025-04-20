c692b1b0e00a651a2cca4642cc5eee935ed8069d
NaimaKhan2110 <naimahomairakhan@gmail.com>
2025-04-20 22:58:30 +0600

    Update admin.py

--- admin.py
+++ admin.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 # TensorFlow Model Prediction
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_plant_disease_model.keras")
+    model = tf.keras.models.load_model("trained_plant_disease_model.h5")
     image = tf.keras.preprocessing.image.load_img(test_image, target_size=(128, 128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])
